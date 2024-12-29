724a524573a992a337d5fe79f85aeb2502b938fd
GozdeSenkal <gozdesenkal2003@gmail.com>
2024-12-29 12:03:44 +0300

    commit

--- app/src/main/java/com/example/yeniproje/ProfileFragment.kt
+++ app/src/main/java/com/example/yeniproje/ProfileFragment.kt
@@ -1,14 +1,26 @@
 package com.example.yeniproje
 
+import RegistrationFragment
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.yeniproje.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +54,48 @@ class ProfileFragment : Fragment() {
                 .commit()
         }
 
+        //google girişi
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val sharedPreferences = requireActivity().getSharedPreferences("userPrefs", AppCompatActivity.MODE_PRIVATE)
+                            sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
+                            Toast.makeText(context, "Giriş başarılı!", Toast.LENGTH_SHORT).show()
+                            // MyProfileFragment'e yönlendir
+                            parentFragmentManager.beginTransaction()
+                                .replace(R.id.fragmentContainerView, MyProfileFragment())
+                                .commit()
+                        } else {
+                            Toast.makeText(context, "Hata: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(context, "Lütfen tüm alanları doldurun.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Firebase Auth başlat
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Google Sign-In yapılandırması
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        // Giriş butonuna tıklama işlemi
+        binding.googleSignInButton.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            launcher.launch(signInIntent)
+        }
+
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -51,6 +105,22 @@ class ProfileFragment : Fragment() {
         (activity as MainActivity).updateToolbarTitle("Profile")
     }
 
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            val account = task.result
+            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
 
+            // Firebase ile giriş
+            firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener { authResult ->
+                    if (authResult.isSuccessful) {
+                        //Toast.makeText(this, "Giriş başarılı!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        //Toast.makeText(this, "Giriş başarısız: ${authResult.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
 }
 
